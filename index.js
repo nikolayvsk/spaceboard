7e5ade7dde441d9817511439757c90bf8d58b794
Nikolay_Vsk <nikolay_vsk@knu.ua>
2022-09-29 02:05:45 +0300

    init

---
+++ index.js
@@ -0,0 +1,52 @@
+const canvas = document.getElementById('drawing-board');
+const toolbar = document.getElementById('toolbar');
+const ctx = canvas.getContext('2d');
+
+const canvasOffsetX = canvas.offsetLeft;
+const canvasOffsetY = canvas.offsetTop;
+
+canvas.width = window.innerWidth - canvasOffsetX;
+canvas.height = window.innerHeight - canvasOffsetY;
+
+let isPainting = false;
+let lineWidth = 1;
+let startX;
+let startY;
+
+toolbar.addEventListener('click', i => {
+    if (i.target.id === 'clear') {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+});
+
+toolbar.addEventListener('change', i => {
+    if(i.target.id === 'lineWidth') {
+        lineWidth = i.target.value;
+    }
+});
+
+const draw = (i) => {
+    if(!isPainting) {
+        return;
+    }
+
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = 'round';
+
+    ctx.lineTo(i.clientX - canvasOffsetX, i.clientY);
+    ctx.stroke();
+}
+
+canvas.addEventListener('mousedown', (i) => {
+    isPainting = true;
+    startX = i.clientX;
+    startY = i.clientY;
+});
+
+canvas.addEventListener('mouseup', i => {
+    isPainting = false;
+    ctx.stroke();
+    ctx.beginPath();
+});
+
+canvas.addEventListener('mousemove', draw);
